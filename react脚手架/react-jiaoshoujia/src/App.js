fb0e3ab7207f12b0b399514e5f17277b92324dc9
李楠林 <2822592790@qq.com>
2022-09-05 21:32:04 +0800

    生命周期

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,32 +1,55 @@
 import React from "react"
-//使用类组件
-// class App extends React.Component{
-//     constructor(){
-//         super()
-//         this.state={
-//             name:'jack'
-//         }
-//     }
+//函数式组件没有生命周期,但是后面可以模拟出来
 
-
-//     render(){
-//         const {name} = this.state
-//         return (
-//             <div>
-//                 <h2>我是类组件{name}</h2>
-//             </div>
-//         )
-//     }
-// }
-
-
-//使用函数式组件
-/* 特点:1.没有this
-2.hooks->解决状态保存问题 */
-function App(){
-    return (
-        <div>我是函数式组件</div>
-    )
+//相当于另外一个组件
+class Cpn extends React.Component{
+    render(){
+        return <div>我是Cpn组件</div> 
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount,此组件被卸载了-Cpn');
+    }
+    componentDidMount(){
+        console.log('Cpn--componentDidMount');
+    }
 }
+
+class App extends React.Component{
+    constructor(){
+        super()
+        console.log('constructor');
+        this.state={
+            counter:0,
+            isShow:true
+        }
+    }
+    render(){
+        console.log('render');
+        return (
+            
+            <div>
+                <h1>当前计数: {this.state.counter}</h1>
+                <button onClick={e=>this.change1()}>+1</button>
+                <button onClick={e=>{this.setState({isShow:!this.state.isShow});console.log(this.state.isShow);}}>切换</button>
+
+                {this.state.isShow && <Cpn/>}
+            </div>
+        )
+    }
+    change1(){
+        this.setState({
+            counter:this.state.counter+1
+        })
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+}
+
+
 
 export default App
