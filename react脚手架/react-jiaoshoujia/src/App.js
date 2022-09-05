3a08156c43835c4d5208010ed89b490563c3f51b
李楠林 <2822592790@qq.com>
2022-09-05 20:23:26 +0800

    类组件与函数组件

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,21 +1,32 @@
 import React from "react"
 //使用类组件
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            name:'jack'
-        }
-    }
+// class App extends React.Component{
+//     constructor(){
+//         super()
+//         this.state={
+//             name:'jack'
+//         }
+//     }
 
 
-    render(){
-        const {name} = this.state
-        return (
-            <div>
-                <h2>我是类组件{name}</h2>
-            </div>
-        )
-    }
+//     render(){
+//         const {name} = this.state
+//         return (
+//             <div>
+//                 <h2>我是类组件{name}</h2>
+//             </div>
+//         )
+//     }
+// }
+
+
+//使用函数式组件
+/* 特点:1.没有this
+2.hooks->解决状态保存问题 */
+function App(){
+    return (
+        <div>我是函数式组件</div>
+    )
 }
+
 export default App
