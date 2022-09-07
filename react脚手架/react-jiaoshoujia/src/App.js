cb4ec0b3eb1208b8da06e1c1d7ca935cc7c93d58
李楠林 <2822592790@qq.com>
2022-09-07 09:32:53 +0800

    setState本身的合并

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -4,26 +4,60 @@ export class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            message: 'hello world',
-            name:'coderwhy'
+            counter: 0,
+            show:'12'
         }
     }
     render() {
         return (
             <div>
-                <h2>{this.state.message}</h2>
-                <h2>{this.state.name}</h2>
-                <button onClick={e => this.changemessage()}>改变文本</button>
+                <h2>{this.state.counter},{this.state.show}</h2>
+                <button onClick={e => this.changecounter()}>改变文本</button>
             </div>
 
         )
     }
-    changemessage() {
-        this.setState({
-            message:'你好 之华'
+    changecounter() {
+        //相同的setState操作是会被合并的
+        // this.setState({
+        //     counter:this.state.counter+1
+        // })
+        // this.setState({
+        //     counter:this.state.counter+1,
+        //     show:'21'
+        // })
+        // this.setState({
+        //     counter:this.state.counter+1
+        // })
+
+        //想要相同的操作不被合并的方法
+        this.setState((prevState,props)=>{
+            return {
+                counter:prevState.counter+1
+            }
         })
-        //setState传入了一个新的对象,不会覆盖掉state对象中的name属性吗?
-        //在react源码中 ---> Object.assgin({},this.state,{message:'你好 之华'})
+        this.setState((prevState,props)=>{
+            return {
+                counter:prevState.counter+1
+            }
+        })
+        this.setState((prevState,props)=>{
+            return {
+                counter:prevState.counter+1
+            }
+        })
+
+
+        /* 
+        总结: 
+        在setState中传入对象的话,多次相同的数据改变是会被最后一次覆盖的, 
+        因为每一次setState都调用了 Object.assgin({},this.state,{counter:this.state.counter+1})
+
+        当setState中传入函数的时候,函数中两个参数prevState与props , 
+        其中的prevState参数在每一次函数执行的时候都会以当下函数的结果作为下一次setState的prevState参数
+        从而实现setState的累加效果
+
+        */
     }
 }
 
