0cbe0bcd85961476cec3b3a4b09d50908ebb9ff3
李楠林 <2822592790@qq.com>
2022-09-07 21:10:12 +0800

    13.受控组件(双向绑定?)

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,64 +1,41 @@
-import React, { PureComponent, createRef } from 'react'
+import React, { PureComponent } from 'react'
 
-class Counter extends PureComponent {
-    constructor(){
-        super()
-        this.state={
-            counter:0
-        }
+export default class App extends PureComponent {
+  constructor(){
+    super()
+    this.state={
+      username:'嘿嘿嘿'
     }
+  }
   render() {
     return (
       <div>
-        <div>{this.state.counter}</div>
-        <button onClick={e=>this.increment()}>+1</button>
+        <form onSubmit={e=>this.formSubmit(e)}>
+          <label htmlFor='username'>
+
+            {/* 这个input就叫做受控组件,感觉有点像是vue中的双向绑定 */}
+            <input 
+            id='username' 
+            type='text' 
+            onChange={e=>this.inputValueChange(e)}
+            value={this.state.username}></input>
+          </label>
+
+          {/* 现在input是默认提交的,但是我们希望在react中监听提交事件所以在form中添加onSubmit */}
+          <input type='submit'></input>
+        </form>
       </div>
     )
   }
-  increment(){
+  formSubmit(event){
+    //取消表单的默认行为
+    event.preventDefault();
+    
+  }
+  inputValueChange(event){
+    console.log(event.target.value);
     this.setState({
-        counter:this.state.counter+1
+      username:event.target.value
     })
   }
-}
-
-
-export default class App extends PureComponent {
-    constructor() {
-        super()
-        this.titleRef = createRef()
-        this.counterRef = createRef()
-        this.titlefunc = null
-    }
-    render() {
-        return (
-            <div>
-
-                {/* //字符串ref------- 弃用 */}
-                {/* <div ref='titleRef'>hello world</div>
-                <button onClick={e=>this.changetext}>改变文本</button> */}
-
-                {/*       //对象ref  ----- 常用 */}
-                <div ref={this.titleRef}>hello world</div>
-
-                {/*       //函数ref */}
-                <div ref={(args)=>this.titlefunc = args}>hello world</div>
-                <button onClick={e => this.changetext()}>改变文本</button>
-
-                {/* ref也可以在组件中使用,跟vue中的ref一样,父组件都可以通过ref调用子组件中的方法 , 但是不可以在函数式组件中使用ref , 因为他们没有实例  */}
-                <Counter ref={this.counterRef}/>
-                <button onClick={e=>this.useCounterfunc()}>点击使用子组件的状态</button>
-            </div>
-        )
-    }
-
-    changetext() {
-        this.titleRef.current.innerText = '你好 之华'
-        console.log(this.titlefunc);
-        this.titlefunc.innerText = "你好 扶华"
-    }
-
-    useCounterfunc(){
-        this.counterRef.current.increment()
-    }
 }
