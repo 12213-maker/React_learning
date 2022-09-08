95099d43d0f2990b4de1c30105327c4f76117248
李楠林 <2822592790@qq.com>
2022-09-08 17:00:29 +0800

    14.高阶组件

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,41 +1,47 @@
 import React, { PureComponent } from 'react'
 
-export default class App extends PureComponent {
-  constructor(){
-    super()
-    this.state={
-      username:'嘿嘿嘿'
-    }
-  }
+ class App extends PureComponent {
   render() {
     return (
-      <div>
-        <form onSubmit={e=>this.formSubmit(e)}>
-          <label htmlFor='username'>
-
-            {/* 这个input就叫做受控组件,感觉有点像是vue中的双向绑定 */}
-            <input 
-            id='username' 
-            type='text' 
-            onChange={e=>this.inputValueChange(e)}
-            value={this.state.username}></input>
-          </label>
-
-          {/* 现在input是默认提交的,但是我们希望在react中监听提交事件所以在form中添加onSubmit */}
-          <input type='submit'></input>
-        </form>
-      </div>
+      <div>App:{this.context.name}</div>
     )
   }
-  formSubmit(event){
-    //取消表单的默认行为
-    event.preventDefault();
-    
-  }
-  inputValueChange(event){
-    console.log(event.target.value);
-    this.setState({
-      username:event.target.value
-    })
-  }
 }
+
+const enhanceFunc = function(WrapperComponent,newConponentName){
+
+  //但是在这里有一个问题就是只要使用这个高阶组件那么返回的新组件的名字永远都叫做NewComponent
+     class NewComponent extends PureComponent{
+      render(){
+        return (
+          <div>
+            高阶组件
+            <WrapperComponent {...this.context}/>
+          </div>
+        )
+      }
+
+    }
+    //但是在这里有一个问题就是只要使用这个高阶组件那么返回的新组件的名字永远都叫做NewComponent
+    //修改这个组件的名字 , 避免每次使用高阶组件输出的组件名字都一样
+    NewComponent.displayName = newConponentName
+
+    return NewComponent
+}
+
+
+
+const newApp = enhanceFunc(App,'Mycomponent')
+
+
+export default newApp
+
+
+
+
+
+
+
+
+
+
--- react脚手架/react-jiaoshoujia/src/index.js
+++ react脚手架/react-jiaoshoujia/src/index.js
@@ -2,7 +2,7 @@
 
 import ReactDOM from "react-dom"
 import React from "react"
-import App from './App'
+import App from './高阶组件-生命周期劫持'
 
 
 ReactDOM.render(<App/>,document.getElementById('root'))
