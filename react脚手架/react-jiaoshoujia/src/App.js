0204d7613579955bfe5ec681a9597ec55c8dc294
李楠林 <2822592790@qq.com>
2022-09-08 19:36:49 +0800

    16.Protals

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,25 +1,29 @@
-import React, { PureComponent ,forwardRef , createRef} from 'react'
+import React, { PureComponent } from 'react'
+import reactDom from 'react-dom'
 
-
-const ComponentFunc = forwardRef(function(props,ref){
-  return (
-    <div ref={ref}>forwardRef</div>
-  )
-})
-
-
-/* ref的转发 forwardRef(高阶组件,实质上是一个函数)  -->用于给函数组件添加ref */
-// 后面我们可以直接用useRef来创建函数的ref
-export default class App extends PureComponent {
-  constructor(){
+/* Protals的使用 , 有什么我们希望一个组件独立于它的父组件出现在任意的位置 */
+class Parent extends PureComponent {
+  constructor() {
     super()
-    this.forref = createRef()
   }
   render() {
     return (
+      reactDom.createPortal(
+        this.props.children,
+        document.getElementById('protals')
+      )
+    )
+  }
+}
+
+export default class App extends PureComponent {
+  render() {
+    return (
       <div>
-        <ComponentFunc ref={this.forref}></ComponentFunc>
-        <button onClick={e=>console.log(this.forref.current)}>点击打印ref</button>
+        App
+        <Parent>
+          我是独立于整个根节点root之外的一个dom节点
+        </Parent>
       </div>
     )
   }
