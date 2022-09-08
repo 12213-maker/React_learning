cf550f60cb0559ce2dfd99023da72d97b36db8ec
李楠林 <2822592790@qq.com>
2022-09-08 19:16:54 +0800

    15.forwardRef

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,47 +1,26 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent ,forwardRef , createRef} from 'react'
 
- class App extends PureComponent {
+
+const ComponentFunc = forwardRef(function(props,ref){
+  return (
+    <div ref={ref}>forwardRef</div>
+  )
+})
+
+
+/* ref的转发 forwardRef(高阶组件,实质上是一个函数)  -->用于给函数组件添加ref */
+// 后面我们可以直接用useRef来创建函数的ref
+export default class App extends PureComponent {
+  constructor(){
+    super()
+    this.forref = createRef()
+  }
   render() {
     return (
-      <div>App:{this.context.name}</div>
+      <div>
+        <ComponentFunc ref={this.forref}></ComponentFunc>
+        <button onClick={e=>console.log(this.forref.current)}>点击打印ref</button>
+      </div>
     )
   }
 }
-
-const enhanceFunc = function(WrapperComponent,newConponentName){
-
-  //但是在这里有一个问题就是只要使用这个高阶组件那么返回的新组件的名字永远都叫做NewComponent
-     class NewComponent extends PureComponent{
-      render(){
-        return (
-          <div>
-            高阶组件
-            <WrapperComponent {...this.context}/>
-          </div>
-        )
-      }
-
-    }
-    //但是在这里有一个问题就是只要使用这个高阶组件那么返回的新组件的名字永远都叫做NewComponent
-    //修改这个组件的名字 , 避免每次使用高阶组件输出的组件名字都一样
-    NewComponent.displayName = newConponentName
-
-    return NewComponent
-}
-
-
-
-const newApp = enhanceFunc(App,'Mycomponent')
-
-
-export default newApp
-
-
-
-
-
-
-
-
-
-
