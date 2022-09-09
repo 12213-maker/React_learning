a16851d2d08795ab53f1f29ebb37e295e53f354e
李楠林 <2822592790@qq.com>
2022-09-09 10:06:25 +0800

    16.css-module

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,30 +1,26 @@
-import React, { PureComponent } from 'react'
-import reactDom from 'react-dom'
-
-/* Protals的使用 , 有什么我们希望一个组件独立于它的父组件出现在任意的位置 */
-class Parent extends PureComponent {
-  constructor() {
-    super()
-  }
-  render() {
-    return (
-      reactDom.createPortal(
-        this.props.children,
-        document.getElementById('protals')
-      )
-    )
-  }
-}
+import React, { PureComponent ,Fragment} from 'react'
+import Appcopy from './css-modules/app'
+import Home from './css-modules/home'
 
 export default class App extends PureComponent {
+  constructor(){
+    super()
+    this.state={
+      counter:0
+    }
+  }
   render() {
     return (
-      <div>
-        App
-        <Parent>
-          我是独立于整个根节点root之外的一个dom节点
-        </Parent>
-      </div>
+      <>
+        <Appcopy></Appcopy>
+        <Home></Home>
+      </>
+
     )
+  }
+  changeCounter(){
+    this.setState({
+      counter:this.state.counter+1
+    })
   }
 }
