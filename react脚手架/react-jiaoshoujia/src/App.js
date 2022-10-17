49ed0b05a52ab7a7b6a208e4021c312aab547282
李楠林 <linanlin@linanlindeMac-mini.local>
2022-10-17 11:01:12 +0800

    new try

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -1,74 +1,9 @@
 import React, { PureComponent } from 'react'
-import axios from 'axios'
-
-
-//这样可以修改axios的默认配置
-axios.defaults.baseURL = "https://httpbin.org";
-axios.defaults.timeout = 5000;
-axios.defaults.headers.common['token'] = '46354';
-axios.defaults.headers.post["Content-type"] ='application/text';
 
 export default class App extends PureComponent {
-
-  async componentDidMount() {
-    // axios({
-    //   url: 'http://httpbin.org/get',
-    //   method: 'get',
-    //   params: {
-    //     name: 'why',
-    //     age: 18
-    //   }
-    // }).then(
-    //   (res) => { console.log(res); },
-    //   (err) => { console.log(err); }
-    // )
-
-
-    // axios.get("http://httpbin.org/get", {
-    //   params: {
-    //     name: 'why',
-    //     age: 18
-    //   }
-    // }).then(
-    //   (res) => { console.log(res); },
-    //   (err) => { console.log(err); }
-    // )
-
-
-    // axios.post('http://httpbin.org/post',{
-    //   data:{
-    //     name: 'why',
-    //     age: 18
-    //   }
-    // }).then(
-    //   (res) => { console.log(res);},
-    //   (err) => { console.log(err);}
-    //   )
-
-
-    //当我们想要捕获错误的时候就可以使用try catch
-    try{
-      const result = await axios.post('http://httpbin.org/post',{
-        data:{
-          name: 'why',
-          age: 18
-        }
-      })
-
-      console.log(result);
-    }catch(err){
-      console.log(err);
-    }
-
-
-  }
-
   render() {
     return (
-      <div>
-
-
-      </div>
+      <div>App</div>
     )
   }
 }
