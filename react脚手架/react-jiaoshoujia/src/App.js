ee9b6a69fbc12c45370032c6f9687a4719f546be
李楠林 <linanlin@bilibili.com>
2023-03-31 15:27:33 +0800

    setState多次调用

--- react脚手架/react-jiaoshoujia/src/App.js
+++ react脚手架/react-jiaoshoujia/src/App.js
@@ -5,7 +5,8 @@ export class App extends React.Component {
         super()
         this.state = {
             message: 'hello world',
-            name:'coderwhy'
+            name: 'coderwhy',
+            counter: 0
         }
     }
     render() {
@@ -13,17 +14,32 @@ export class App extends React.Component {
             <div>
                 <h2>{this.state.message}</h2>
                 <h2>{this.state.name}</h2>
+                <h2>{this.state.counter}</h2>
                 <button onClick={e => this.changemessage()}>改变文本</button>
+                <button onClick={e => this.Counteradd()}>counter++</button>
+
             </div>
 
         )
     }
     changemessage() {
         this.setState({
-            message:'你好 之华'
+            message: '你好 之华'
         })
         //setState传入了一个新的对象,不会覆盖掉state对象中的name属性吗?
         //在react源码中 ---> Object.assgin({},this.state,{message:'你好 之华'})
+    }
+    Counteradd() {
+        //这样就不会在异步更新队列里面合并成一次更新
+        this.setState((prevState) => {
+            return { counter: prevState.counter + 1 }
+        })
+        this.setState((prevState) => {
+            return { counter: prevState.counter + 1 }
+        })
+        this.setState((prevState) => {
+            return { counter: prevState.counter + 1 }
+        })
     }
 }
 
