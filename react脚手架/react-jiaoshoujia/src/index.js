0d7c0a33ac0face592c3e0d4d2704697420d14cf
linanlin <linanlin@meituan.com>
2023-06-06 15:42:32 +0800

    clear

--- react脚手架/react-jiaoshoujia/src/index.js
+++ react脚手架/react-jiaoshoujia/src/index.js
@@ -1,10 +1,8 @@
 // 这个文件的功能是渲染挂载
 import React, { createRoot } from "react-dom/client"
 import App from './App'
-import 'antd/dist/antd.less';
-import { BrowserRouter } from "react-router-dom";
 
 // 注意：这里使用的 React18 的写法
 const root = createRoot(document.getElementById('root'));
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(<App />);
 
--- react脚手架/react-jiaoshoujia/src/store/index.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import uesrSlice from './reducer/userRducer.jsx'
-
-export const store = configureStore({
-  reducer: {
-    user: uesrSlice
-  }
-})
